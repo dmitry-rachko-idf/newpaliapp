6aec3740b30f201b50cb122e577895870e650be4
dmitry-rachko-idf <dmitry.rachko@idfinance.com>
2022-03-04 11:33:46 +0300

    fix according to the comments

--- packages/data/lib/di/injector.dart
+++
@@ -1,6 +0,0 @@
-// import 'package:data/di/injector.config.dart';
-// import 'package:get_it/get_it.dart';
-// import 'package:injectable/injectable.dart';
-
-// @InjectableInit()
-// void configureDataDependencies(GetIt getIt) => $initGetIt(getIt);
--- packages/domain/lib/di/injector.dart
+++
@@ -1,6 +0,0 @@
-// import 'package:domain/di/injector.config.dart';
-// import 'package:get_it/get_it.dart';
-// import 'package:injectable/injectable.dart';
-
-// @InjectableInit()
-// void configureDomainDependencies(GetIt getIt) => $initGetIt(getIt);
